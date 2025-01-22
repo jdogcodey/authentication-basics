98aede859d6ce15cd65a8bd19a0ac85b32796491
jdogcodey <jonnydawson@hotmail.com>
2025-01-22 23:00:54 +0000

    add sign up form

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
 
 require("dotenv").config();
 const PORT = process.env.PORT;
@@ -21,23 +22,94 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => res.render("index", { user: req.user }));
 
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
-app.post("/sign-up", async (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-    if (err) {
-      console.log(err);
-    } else {
-      try {
-        await pool.query(
-          "INSERT INTO users (username, password) VALUES ($1, $2)",
-          [req.body.username, hashedPassword]
-        );
-        res.redirect("/");
-      } catch (err) {
-        return next(err);
+
+app.post(
+  "/sign-up",
+  [
+    body("first_name")
+      .trim()
+      .notEmpty()
+      .withMessage("First name is required")
+      .isAlpha()
+      .withMessage("First name must only contain letters"),
+    body("last_name")
+      .trim()
+      .notEmpty()
+      .withMessage("Last name is required")
+      .isAlpha()
+      .withMessage("Last name must only contain letters"),
+    body("username").trim().notEmpty().withMessage("Username is required"),
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("Must be a valid email")
+      .normalizeEmail(),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters long")
+      .matches(/[A-Z]/)
+      .withMessage("Password must contain at least one uppercase letter")
+      .matches(/[a-z]/)
+      .withMessage("Password must contain at least one lowercase letter")
+      .matches(/[0-9]/)
+      .withMessage("Password must contain at least one number")
+      .matches(/[@$!%*?&]/)
+      .withMessage(
+        "Password must contain at least one special character (@$!%*?&)"
+      )
+      .not()
+      .isIn(["password", "123456", "qwerty"])
+      .withMessage("Password is too common")
+      .trim()
+      .escape(),
+    body("confirm-password").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password confirmation does not match password");
       }
+      return true;
+    }),
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  });
-});
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE email = $1 OR username = $2",
+        [req.body.email, req.body.username]
+      );
+      if (rows.length > 0) {
+        const duplicateField =
+          rows[0].email === req.body.email ? "email" : "username";
+        return res.status(400).json({
+          errors: [
+            {
+              msg: `${duplicateField} is already in use`,
+              param: duplicateField,
+            },
+          ],
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+      await pool.query(
+        "INSERT INTO users (first_name, last_name, username, email, password ) VALUES ($1, $2, $3, $4, $5)",
+        [
+          req.body.first_name,
+          req.body.last_name,
+          req.body.username,
+          req.body.email,
+          hashedPassword,
+        ]
+      );
+      res.redirect("/");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
